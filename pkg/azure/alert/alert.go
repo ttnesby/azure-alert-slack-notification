9453dda41c404b91547468b145a360d422dcf90a
Torstein Nesby <ttnesby@gmail.com>
2023-09-11 21:28:58 +0200

    wip - added alert spec

---
+++ pkg/azure/alert/alert.go
@@ -0,0 +1,33 @@
+package alert
+
+// https://learn.microsoft.com/en-us/azure/azure-monitor/alerts/alerts-common-schema#sample-alert-payload
+
+type Content struct {
+	AlertId             string   `json:"alertId"`
+	AlertRule           string   `json:"alertRule"`
+	Severity            string   `json:"severity"`
+	SignalType          string   `json:"signalType"`
+	MonitorCondition    string   `json:"monitorCondition"`
+	MonitoringService   string   `json:"monitoringService"`
+	AlertTargetIDs      []string `json:"alertTargetIDs"`
+	ConfigurationItems  []string `json:"configurationItems"`
+	OriginAlertId       string   `json:"originAlertId"`
+	FiredDateTime       string   `json:"firedDateTime"`
+	ResolvedDateTime    string   `json:"resolvedDateTime"`
+	Description         string   `json:"description"`
+	EssentialsVersion   string   `json:"essentialsVersion"`
+	AlertContextVersion string   `json:"alertContextVersion"`
+}
+
+type Essentials struct {
+	Essentials Content `json:"essentials"`
+}
+
+type CommonAlertSchema struct {
+	SchemaId string     `json:"schemaId"`
+	Data     Essentials `json:"data"`
+}
+
+func Parse(json string) *CommonAlertSchema {
+	return &CommonAlertSchema{}
+}
