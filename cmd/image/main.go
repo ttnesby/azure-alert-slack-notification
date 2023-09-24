df9b1fcfa9e9b347aea9adc3d500fae79035635d
Torstein Nesby <ttnesby@gmail.com>
2023-09-24 03:00:57 +0200

    sucessful build cust caddy with dagger

--- cmd/image/main.go
+++ cmd/image/main.go
@@ -29,7 +29,7 @@ func build(ctx context.Context) error {
 	defer client.Close()
 
 	// get reference to the local project
-	src := client.Host().Directory(".")
+	src := client.Host().Directory("./cmd/caddy")
 
 	// create empty directory to put build outputs
 	outputs := client.Directory()
@@ -37,11 +37,10 @@ func build(ctx context.Context) error {
 	// get `golang` image
 	golang := client.
 		Container().
-		From("golang:latest").
-		WithExec([]string{"go", "install", "github.com/caddyserver/xcaddy/cmd/xcaddy@latest"})
+		From("golang:latest")
 
 	// mount cloned repository into `golang` image
-	golang = golang.WithDirectory("./", src).WithWorkdir("./")
+	golang = golang.WithDirectory("/src", src).WithWorkdir("/src")
 
 	for _, goos := range oses {
 		for _, goarch := range arches {
@@ -53,18 +52,7 @@ func build(ctx context.Context) error {
 			build = build.WithEnvVariable("GOARCH", goarch)
 
 			// build application caddy with extensions
-			build = build.WithExec([]string{
-				"xcaddy",
-				"build",
-				"--with",
-				"github.com/ttnesby/slack-block-builder/caddy-ext/azalertslacknotification",
-				"--with",
-				"github.com/corazawaf/coraza-caddy/v2",
-				"--with",
-				"github.com/mholt/caddy-ratelimit",
-				// "-o",
-				// path,
-			})
+			build = build.WithExec([]string{"go", "build", "-o", path})
 
 			// get reference to build output directory in container
 			outputs = outputs.WithDirectory(path, build.Directory(path))
